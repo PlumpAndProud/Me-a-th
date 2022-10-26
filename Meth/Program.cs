38943db5c85adb2dd403f22ae7aa9a0cd79a1f0b
PlumpAndProud <f.machi03@gmail.com>
2022-10-26 10:49:29 +0200

    v1.0.2
    +no out of scope for int B)

--- Meth/Program.cs
+++ Meth/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq; // for Count
+using System.Collections.Generic; //for List<>
 
 namespace Meth
 {
@@ -10,11 +11,13 @@ namespace Meth
 
             //1st initialization 
 
+            int variable = 0;
+
             Console.WriteLine("Halo watchu want?");
             Console.WriteLine("1. Simple calculator!");
             Console.WriteLine("69. Close program!");
 
-            int variable = Int32.Parse(Console.ReadLine());
+            Int32.TryParse(Console.ReadLine(), out variable);
 
             short error = 3;
 
@@ -25,7 +28,9 @@ namespace Meth
                     Console.WriteLine("So watchu want?");
                     Console.WriteLine("1. Simple calculator!");
                     Console.WriteLine("69. Close program!");
-                    
+
+                    Int32.TryParse(Console.ReadLine(), out variable);
+
                 }
                 else if(error == 0)
                 {
@@ -33,9 +38,10 @@ namespace Meth
                     Console.WriteLine("1. No?");
                     Console.WriteLine("2. Yes?");
 
+                    Int32.TryParse(Console.ReadLine(), out variable);
+
                 }
 
-                variable = Int32.Parse(Console.ReadLine());
                 if (variable == 2)
                 {
                     Environment.Exit(0);
@@ -55,7 +61,6 @@ namespace Meth
         // short error: 
         //      0 - no error
         //      1 - error
-        //      69 - end programme
 
         public static short MyPurpose(int variable)
         {
@@ -83,10 +88,17 @@ namespace Meth
             string userInput = Console.ReadLine();
 
             while (operation == 0)
-            {   
+            {
+                var list = new List<char>();
+
                 char[] userInputArray = userInput.ToCharArray();
 
-                operation = whatOperation(userInputArray);
+                for(int i = 0; i < userInput.Length; i++)
+                {
+                    list.Add(userInputArray[i]);
+                }
+
+                operation = whatOperation(list);
 
                 if (operation == 0)
                 {
@@ -96,7 +108,7 @@ namespace Meth
             }
         }
 
-        // whatOperation
+        // whatOperation    https://stackoverflow.com/questions/1836959/how-to-add-to-end-of-array-c
         //
         // Input: 
         // userInput - user input of equation eg. 12 + 12
@@ -109,63 +121,70 @@ namespace Meth
         //      3 - * 
         //      4 - / 
         //      5 - ^
-        public static short whatOperation(char[] userInput)
+        public static short whatOperation(List<char> list)
         {
-            int numberOfInput = userInput.Count();
+            int numberOfInput = list.Count();
 
-                for(int i = 0; i < numberOfInput; i++)
+            list.Add('.');
+
+            char[] userInput = list.ToArray();
+
+            for (int i = 0; i < numberOfInput; i++)
+            {
+                if (userInput[i + 1] == ' ' && userInput[i + 1] != '.')
                 {
-                    
-                switch (userInput[i])
+                    switch (userInput[i])
                     {
 
                         case '+':
                             {
-                                if(userInput[i + 1] == ' ')
+                                if (userInput[i + 1] == ' ')
                                 {
-                                Console.WriteLine("Its addition");
-                                return 1;
+                                    Console.WriteLine("Its addition");
+                                    return 1;
                                 }
-                            break;
+                                break;
                             }
                         case '-':
                             {
-                            if (userInput[i + 1] == ' ')
+                                if (userInput[i + 1] == ' ')
                                 {
-                                Console.WriteLine("Its subtraction");
-                                return 2;
+                                    Console.WriteLine("Its subtraction");
+                                    return 2;
                                 }
-                            break;
+                                break;
                             }
                         case '*':
                             {
-                            if (userInput[i + 1] == ' ')
-                            {
-                                Console.WriteLine("Its multiplication");
-                                return 3;
-                            }  
-                            break;
+                                if (userInput[i + 1] == ' ')
+                                {
+                                    Console.WriteLine("Its multiplication");
+                                    return 3;
+                                }
+                                break;
                             }
                         case '/':
                             {
-                            if (userInput[i + 1] == ' ')
-                            {
-                                Console.WriteLine("Its division");
-                                return 4;
-                            }
-                            break;
+                                if (userInput[i + 1] == ' ')
+                                {
+                                    Console.WriteLine("Its division");
+                                    return 4;
+                                }
+                                break;
                             }
                         case '^':
                             {
-                            if (userInput[i + 1] == ' ')
-                            {
-                                Console.WriteLine("Its powering");
-                                return 5;
-                            }
-                            break;
+                                if (userInput[i + 1] == ' ')
+                                {
+                                    Console.WriteLine("Its powering");
+                                    return 5;
+                                }
+                                break;
                             }
                     }
-                }
+            }
+
+            }
             
             return 0;
         }
